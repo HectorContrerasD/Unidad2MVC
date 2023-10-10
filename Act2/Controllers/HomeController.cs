0eb3865f3f56c55714fca1eb1504b6eb0bcb8067
HectorContrerasD <201G0239@rcarbonifera.tecnm.mx>
2023-10-10 15:11:02 -0600

    Mas avances

---
+++ Act2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Act2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult PorPais()
+        {
+            return View();
+        }
+        public IActionResult Detalles()
+        { 
+            return View();
+        }
+    }
+}
